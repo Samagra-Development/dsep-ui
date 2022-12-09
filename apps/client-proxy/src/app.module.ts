f9946134266d3d92463d159edadd8f0d0c153334
Yash Mittal <mittal.yash12@hotmail.com>
2022-12-10 00:01:17 +0530

    Feat: Transferred Client Proxy from DSEP-APIs to here
    Signed-off-by: Yash Mittal <mittal.yash12@hotmail.com>

---
+++ apps/client-proxy/src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AppGateway } from './app.gateway';
+import { SearchService } from './search/search.service';
+import { SearchModule } from './search/search.module';
+import { HttpModule } from '@nestjs/axios';
+
+@Module({
+  imports: [HttpModule, SearchModule],
+  controllers: [AppController],
+  providers: [AppService, AppGateway, SearchService],
+})
+export class AppModule {}
