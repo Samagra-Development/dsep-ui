4d2b0b0baa9a2e5a3039fe0b153a961db4c3fea4
Prateek Jakhar <prtkjakhar@gmail.com>
2023-02-10 11:55:14 +0530

    minor ui changes

--- src/components/Filters/index.tsx
+++ src/components/Filters/index.tsx
@@ -1,20 +1,28 @@
 import { FC } from "react";
-import { MdFilterListAlt } from "react-icons/md";
+import { HiAdjustmentsHorizontal } from "react-icons/hi2";
 import CourseFilters from "../CourseFilters";
 
 const Filters: FC<{ applyFilter: any; mode: any }> = ({
   applyFilter,
   mode,
 }) => {
+  function handleFilter(){
+    let filterDiv = document.getElementById('filterDiv');
+    if(filterDiv?.classList.contains('hidden')){
+      filterDiv.classList.remove('hidden');
+    }else{
+      filterDiv?.classList.add('hidden');
+    }
+  }
   return (
     <div className="col-span-1">
-      <div className={`${mode === "dark" ? "text-white" : ""} font-bold flex items-center gap-2 mb-3`}>
-        <MdFilterListAlt />
-        <div className={`${mode === "dark" ? "text-white" : ""}`}>
-          Filter by
+      <div onClick={handleFilter} className={`font-medium flex items-center gap-2 mb-3 border max-w-fit px-2 rounded-lg bg-[#DADBE0] cursor-pointer`}>
+        <HiAdjustmentsHorizontal />
+        <div className={``}>
+          Filter
         </div>
       </div>
-      <div className="bg-white border border-gray-300 rounded py-5">
+      <div id='filterDiv' className="lg:block hidden">
         <CourseFilters applyFilter={applyFilter} />
       </div>
     </div>
